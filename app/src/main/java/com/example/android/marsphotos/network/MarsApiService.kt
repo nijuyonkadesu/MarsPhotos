9839b10a3d8d3124a38ad7525002077bc4d61cee
nijuyonkadesu <24ksriramv@gmail.com>
2022-10-04 16:44:00 +0530

    setting up retro fit using singleton, Interface. also coroutine compatible

---
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,30 @@
+package com.example.android.marsphotos.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+// retrofit returns json respons: String
+// to work with JSON, need ConverterFactory -> to work with Primitive datatype
+// need ScalarsConverterFactory object
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+// Defines how Retrofit talks with web server using HTTP requests
+interface MarsApiService {
+    @GET("photos")
+    suspend fun getPhotos(): String{
+        return ""// TODO: complete
+    }
+}
+// Singleton - single object, available in public, new obj won't be created on accessing
+// MarsApi.retrofitService the second time
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
