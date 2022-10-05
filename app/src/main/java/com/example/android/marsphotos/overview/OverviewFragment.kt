606d7c09232252b91437c19a0c166dd787d5f3c0
nijuyonkadesu <24ksriramv@gmail.com>
2022-10-05 19:29:48 +0530

    setting up grid layout xml

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -39,9 +39,9 @@ class OverviewFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        //val binding = FragmentOverviewBinding.inflate(inflater)
-        val binding = GridViewItemBinding.inflate(inflater)
+    ): View {
+        val binding = FragmentOverviewBinding.inflate(inflater)
+//        val binding = GridViewItemBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -31,12 +31,11 @@ class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData that stores the status of the most recent request
     private val _status = MutableLiveData<String>()
-
-    private val _photos = MutableLiveData<MarsPhoto>()
-    val photos: LiveData<MarsPhoto> = _photos
-
-    // The external immutable LiveData for the request status
     val status: LiveData<String> = _status
+
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+    val photos: LiveData<List<MarsPhoto>> = _photos
+
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
      */
@@ -51,9 +50,8 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsPhotos() {
         viewModelScope.launch {
             try {
-//                val listResult = MarsApi.retrofitService.getPhotos()
-                _photos.value = MarsApi.retrofitService.getPhotos()[0]
-                _status.value = "   First Mars image URL : ${_photos.value!!.imgSrcUrl}"
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = "Success: Mars properties retrieved"
             }
             catch (e: Exception){
                 _status.value = "Failure: ${e.message}"
